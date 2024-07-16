93518ce8d82a04e7513b0d25cce0b268c1a6973b
ParasBisht21 <bishtparas2003@gmail.com>
2024-07-16 11:06:17 +0530

    Project Card Animated

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { CardContainer, CardBody, CardItem } from '../sections/Projects/HoverEffectComponents';
+import styles from '../sections/Projects/ProjectStyles.module.css';
 
-function ProjectCard({src,link,text1,des}) {
+function ProjectCard({ src, link, text1, des }) {
   return (
-    <div>
-      <a href={link} target='_blank'>
-          <img className="hover" src={src} alt={'${text1}logo'} />
-          <h3>{text1}</h3>
-          <p>{des}</p>
-        </a>
-    </div>
-  )
+    <a href={link} target="_blank" rel="noopener noreferrer" className={styles.cardLink}>
+      <CardContainer className={styles.card}>
+        <CardBody>
+          <CardItem translateZ={30}>
+            <img className={styles.image} src={src} alt={`${text1} logo`} />
+          </CardItem>
+          <CardItem translateZ={20}>
+            <div className={styles.textContainer}>
+              <h3>{text1}</h3>
+              <p>{des}</p>
+            </div>
+          </CardItem>
+        </CardBody>
+      </CardContainer>
+    </a>
+  );
 }
 
 export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './ProjectStyles.module.css';
 import trackerImg from '../../assets/RealTime_Tracker.jpg';
-import one from "../../assets/one.png"
+import one from '../../assets/one.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -9,9 +9,18 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className={styles.sectionTitle}>Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard src={trackerImg} link='https://github.com/ParasBisht21/RealTime_Tracker' text1="Real Time Tracker"
-         des="Multi Device Location Tracker"/>
-        <ProjectCard src={one} link="https://github.com/ParasBisht21/OneHealth" text1="One Health" des="Unified Health Interface for All"/>
+        <ProjectCard 
+          src={trackerImg} 
+          link='https://github.com/ParasBisht21/RealTime_Tracker' 
+          text1="Real Time Tracker" 
+          des="Multi Device Location Tracker" 
+        />
+        <ProjectCard 
+          src={one} 
+          link="https://github.com/ParasBisht21/OneHealth" 
+          text1="One Health" 
+          des="Unified Health Interface for All" 
+        />
       </div>
     </section>
   );
